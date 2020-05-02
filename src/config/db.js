5253f55d87fbe8a82625dbd90d349072a867a979
Theux17 <oliveira.ms18@gmail.com>
2020-05-02 02:44:12 -0300

    Configurando conexão do banco de dados

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg")
+
+module.exports = new Pool({
+    user: "matheus",
+    password: "puyolsilva17",
+    host: "localhost",
+    port: 5432,
+    database: "gymmanager"
+})
